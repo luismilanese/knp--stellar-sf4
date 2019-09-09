3eb998b0567941e495271853375b065d60cbc930
Luis Milanese <luis.milanese@gmail.com>
2019-09-08 21:48:37 -0300

    bundle configs

--- src/Controller/ArticleController.php
+++ src/Controller/ArticleController.php
@@ -3,6 +3,7 @@ namespace App\Controller;
 
 use Michelf\MarkdownInterface;
 use Psr\Log\LoggerInterface;
+use Symfony\Component\Cache\Adapter\AdapterInterface;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -21,7 +22,7 @@ class ArticleController extends AbstractController
     /**
      * @Route("/news/{slug}", name="article_show")
      */
-    public function show($slug, MarkdownInterface $markdown)
+    public function show($slug, MarkdownInterface $markdown, AdapterInterface $cache)
     {
         $comments = [
             'I ate a normal rock once. It did NOT taste like bacon!',
@@ -32,13 +33,19 @@ class ArticleController extends AbstractController
         $articleContent = <<<EOF
 Bacon ipsum dolor amet **burgdoggen tail porchetta**, bacon salami ground round pancetta landjaeger spare ribs turkey [filet mignon](https://filet.com) pig kevin meatball. Biltong picanha landjaeger pork loin t-bone venison salami meatball doner, jowl pig swine. Prosciutto burgdoggen sirloin, turducken swine filet mignon jowl leberkas tongue kevin bresaola turkey ribeye brisket fatback. Tongue filet mignon bacon porchetta, beef ribs meatloaf short loin fatback burgdoggen shank turkey. Fatback filet mignon brisket, andouille burgdoggen drumstick bacon jerky. Pork chop tenderloin beef ham hock sausage. Sausage boudin venison turkey, tenderloin andouille pork bacon jerky kevin filet mignon tail biltong.
 
-Alcatra pastrami chuck tri-tip. Shankle chicken pork jerky meatloaf pastrami kevin. Filet mignon kielbasa leberkas, sirloin pancetta boudin ground round turkey meatball pastrami ham hock porchetta cupim pork salami. Jerky pork pastrami chicken picanha, swine bresaola. Corned beef beef ribs picanha alcatra fatback. Ribeye pancetta beef ribs biltong sausage.
+Alcatra pastrami chuck **tri-tip**. Shankle chicken pork jerky meatloaf pastrami kevin. Filet mignon kielbasa leberkas, sirloin pancetta boudin ground round turkey meatball pastrami ham hock porchetta cupim pork salami. Jerky pork pastrami chicken picanha, swine bresaola. Corned beef beef ribs picanha alcatra fatback. Ribeye pancetta beef ribs biltong sausage.
 EOF;
+
+        $cacheItem = $cache->getItem('markdown_'.md5($articleContent));
+        if (!$cacheItem->isHit()) {
+            $cacheItem->set($markdown->transform($articleContent));
+            $cache->save($cacheItem);
+        }
 
 
         return $this->render('article/show.html.twig', [
             'title' => ucwords(str_replace('-', ' ', $slug)),
-            'articleContent' => $markdown->transform($articleContent),
+            'articleContent' => $cacheItem->get(),
             'slug' => $slug,
             'comments' => $comments
         ]);
